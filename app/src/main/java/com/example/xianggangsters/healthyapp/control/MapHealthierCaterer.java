063e234c1ef3e79fb75cc64542f422f6b187020c
peetreatr <peetreatr@gmail.com>
2018-11-01 22:37:12 +0800

    Update MapHealthierCaterer.java

--- app/src/main/java/com/example/xianggangsters/healthyapp/control/MapHealthierCaterer.java
+++ app/src/main/java/com/example/xianggangsters/healthyapp/control/MapHealthierCaterer.java
@@ -1,6 +1,18 @@
 package com.example.xianggangsters.healthyapp.control;
 /*
-This class
+This class displays healthier caterers on google maps
  */
 public class MapHealthierCaterer {
+  //map - googlemap
+  //caterer - kmllayer
+  //addmap - button
+  //remove map - button 
+  
+  void addMapListener(){
+    
+  }
+  
+  void removeMapListener(){
+		
+	}
 }
