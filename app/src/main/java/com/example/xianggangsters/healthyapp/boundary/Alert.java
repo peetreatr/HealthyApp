6f48a4523dfcac714d401bcd2f9c9376f1de9443
peetreatr <peetreatr@gmail.com>
2018-11-01 22:47:31 +0800

    Update Alert.java

--- app/src/main/java/com/example/xianggangsters/healthyapp/boundary/Alert.java
+++ app/src/main/java/com/example/xianggangsters/healthyapp/boundary/Alert.java
@@ -1,8 +1,23 @@
 package com.example.xianggangsters.healthyapp.boundary;
 
 /*
-This class
+This class allows the user to choose which alerts to enable
  */
 public class Alert {
-
+  //alertPM25 - checkbox
+  //alertPSI - checkbox
+  //alertUVI - checkbox
+  
+  
+  void PM25Listener(){
+    
+  }
+  
+  void PSIListener(){
+		
+	}
+  
+  void UVIListener(){
+		
+	}
 }
