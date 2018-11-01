5ee9af5ee7696353f65d80c2c79d0555b4daa546
peetreatr <peetreatr@gmail.com>
2018-11-01 22:38:46 +0800

    Update Calendar.java

--- app/src/main/java/com/example/xianggangsters/healthyapp/control/Calendar.java
+++ app/src/main/java/com/example/xianggangsters/healthyapp/control/Calendar.java
@@ -1,6 +1,9 @@
 package com.example.xianggangsters.healthyapp.control;
 /*
-This class
+This class is used to put reminders in the users calendar
  */
 public class Calendar {
+  void queryCalendar(){
+    
+  }
 }
