aa6a117f79edbcec7b32dd3b83628b5d5350c6e6
peetreatr <peetreatr@gmail.com>
2018-11-01 22:33:10 +0800

    Update MapPSI.java

--- app/src/main/java/com/example/xianggangsters/healthyapp/control/MapPSI.java
+++ app/src/main/java/com/example/xianggangsters/healthyapp/control/MapPSI.java
@@ -1,6 +1,16 @@
 package com.example.xianggangsters.healthyapp.control;
 /*
-This class
+This class uses the getPSI() function to display PSI markers on google map
  */
 public class MapPSI {
+  //map - google map
+  //location manager
+  //location listener
+  //marker
+  
+  public PSI getPSI()
+  {
+    PSI list[];
+    return list;
+  }
 }
