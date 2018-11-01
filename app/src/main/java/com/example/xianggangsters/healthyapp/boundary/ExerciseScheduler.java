fcfd857dcfa5899c8a2f81f7a0d478ef4a3ba79b
peetreatr <peetreatr@gmail.com>
2018-11-01 22:51:07 +0800

    Update ExerciseScheduler.java

--- app/src/main/java/com/example/xianggangsters/healthyapp/boundary/ExerciseScheduler.java
+++ app/src/main/java/com/example/xianggangsters/healthyapp/boundary/ExerciseScheduler.java
@@ -3,4 +3,15 @@ package com.example.xianggangsters.healthyapp.boundary;
 This class
  */
 public class ExerciseScheduler {
+  //scheduler - button
+  //gymLocation - button
+  
+  
+  void schedulerListener(){
+    
+  }
+  
+  void gymListener(){
+		
+	}
 }
