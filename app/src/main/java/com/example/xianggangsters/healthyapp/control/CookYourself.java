048350f00907b31a181ecdaaf3730352ef454b34
peetreatr <peetreatr@gmail.com>
2018-11-01 22:20:36 +0800

    Update CookYourself.java

--- app/src/main/java/com/example/xianggangsters/healthyapp/control/CookYourself.java
+++ app/src/main/java/com/example/xianggangsters/healthyapp/control/CookYourself.java
@@ -1,6 +1,17 @@
 package com.example.xianggangsters.healthyapp.control;
 /*
-This class
+This class uses the getHCSproducts function to list items for the user in order to add to a purchasing cart
  */
 public class CookYourself {
+	//product checkbox
+	//viewCart: textView
+	
+	HCSProduct cart = HCSProduct[];
+	void addToCart(){
+		
+	}
+	
+	void removeFromCart(){
+		
+	}
 }
