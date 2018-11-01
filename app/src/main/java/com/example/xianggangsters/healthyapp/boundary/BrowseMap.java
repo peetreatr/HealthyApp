4b068b5ec63bdf7d87fd620cf5892db067600dca
peetreatr <peetreatr@gmail.com>
2018-11-01 22:50:16 +0800

    Update BrowseMap.java

--- app/src/main/java/com/example/xianggangsters/healthyapp/boundary/BrowseMap.java
+++ app/src/main/java/com/example/xianggangsters/healthyapp/boundary/BrowseMap.java
@@ -4,4 +4,27 @@ package com.example.xianggangsters.healthyapp.boundary;
 This class
  */
 public class BrowseMap {
+
+  //browsePM25 - button
+  //browsePSI - button
+  //browseUVI - button
+  //browseCaterer - button
+  
+  
+  void PM25Listener(){
+    
+  }
+  
+  void PSIListener(){
+		
+	}
+  
+  void catererListener(){
+		
+	}
+  
+  void gymListener(){
+		
+	}
+
 }
