61c2b07c29f28e3b138fe07580a9524e96f2d359
peetreatr <peetreatr@gmail.com>
2018-11-01 22:45:35 +0800

    Update HealthierDiet.java

--- app/src/main/java/com/example/xianggangsters/healthyapp/boundary/HealthierDiet.java
+++ app/src/main/java/com/example/xianggangsters/healthyapp/boundary/HealthierDiet.java
@@ -1,6 +1,18 @@
 package com.example.xianggangsters.healthyapp.boundary;
 /*
-This class
+This class presents the data fetched by the CookYourself class 
  */
 public class HealthierDiet {
+  //home - button
+  //out - button
+  
+ 
+  void cookListener(){
+    
+  }
+  
+  void homeListener(){
+		
+	}
+  
 }
