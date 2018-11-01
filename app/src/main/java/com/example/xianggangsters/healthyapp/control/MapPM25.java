d922f86c2f3b29947a30920aa96029fd7dbc1a92
peetreatr <peetreatr@gmail.com>
2018-11-01 22:23:54 +0800

    Update MapPM25.java

--- app/src/main/java/com/example/xianggangsters/healthyapp/control/MapPM25.java
+++ app/src/main/java/com/example/xianggangsters/healthyapp/control/MapPM25.java
@@ -1,6 +1,10 @@
 package com.example.xianggangsters.healthyapp.control;
 /*
-This class
+This class displays the map and markers to locate places to purchase PM25 masks
  */
 public class MapPM25 {
+  //map - google map
+  //location manager
+  //location listener
+  //marker
 }
