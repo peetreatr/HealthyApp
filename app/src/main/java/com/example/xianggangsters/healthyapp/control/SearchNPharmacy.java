a41e1d507836c688938ebff717c5401060fec825
peetreatr <peetreatr@gmail.com>
2018-11-01 22:40:59 +0800

    Update SearchNPharmacy.java

--- app/src/main/java/com/example/xianggangsters/healthyapp/control/SearchNPharmacy.java
+++ app/src/main/java/com/example/xianggangsters/healthyapp/control/SearchNPharmacy.java
@@ -1,6 +1,19 @@
 package com.example.xianggangsters.healthyapp.control;
 /*
-This class
+This class uses the getLicensedPharmacy() function to display a list of nearby pharmacies
  */
 public class SearchNPharmacy     {
+  //input -textview
+  //search - button 
+  // display - textview
+  
+  licensedPharmacy getLicensedPharmacy()
+  {
+   licensedPharmacy list[];
+    return list;
+  }
+  
+  void searchListener(){
+    
+  }
 }
