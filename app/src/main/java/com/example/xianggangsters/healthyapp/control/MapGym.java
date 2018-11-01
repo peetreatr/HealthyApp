f445896a9032971d85e4e8f891fc3b6b5eb1e89c
peetreatr <peetreatr@gmail.com>
2018-11-01 22:38:03 +0800

    Update MapGym.java

--- app/src/main/java/com/example/xianggangsters/healthyapp/control/MapGym.java
+++ app/src/main/java/com/example/xianggangsters/healthyapp/control/MapGym.java
@@ -1,6 +1,18 @@
 package com.example.xianggangsters.healthyapp.control;
 /*
-This class
+This class displays nearby gyms on google maps for the user
  */
 public class MapGym {
+  //map - googlemap
+  //gym - kmllayer
+  //addmap - button
+  //remove map - button 
+  
+  void addMapListener(){
+    
+  }
+  
+  void removeMapListener(){
+		
+	}
 }
