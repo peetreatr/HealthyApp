964be4c87095b62c6f808b58505e569b426d1ed7
peetreatr <peetreatr@gmail.com>
2018-11-01 22:42:34 +0800

    Update SearchTherapeuticProducts.java

--- app/src/main/java/com/example/xianggangsters/healthyapp/control/SearchTherapeuticProducts.java
+++ app/src/main/java/com/example/xianggangsters/healthyapp/control/SearchTherapeuticProducts.java
@@ -1,6 +1,13 @@
 package com.example.xianggangsters.healthyapp.control;
 /*
-This class
+This class uses the getTherapeuticProducts() function to display a list of therapeutic products
  */
 public class SearchTherapeuticProducts {
+  //input - textview
+  //search - button
+  //display - textview
+  
+  void searchListener{
+    
+  }
 }
